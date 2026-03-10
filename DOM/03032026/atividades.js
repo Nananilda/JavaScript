210fb092e75e92ccfa6b0914662a6b34889d694c
Nananilda <anna.frezarini@aluno.senai.br>
2026-03-10 15:57:00 -0300

    atividades

---
+++ DOM/03032026/atividades.js
@@ -0,0 +1,89 @@
+// Atividade 1
+let texto1 = document.querySelector(".texto1");
+let azul = document.querySelector("#azul");
+let vermelho = document.querySelector("#vermelho");
+
+azul.addEventListener("click", function(){
+    texto1.style.color = "blue";
+});
+
+vermelho.addEventListener("click", function(){
+    texto1.style.color = "red";
+});
+
+
+// Atividade 2
+let MoEs = document.getElementById("MoEs");
+let texto2 = document.getElementById("texto2");
+
+MoEs.addEventListener('click', function() {
+    texto2.classList.toggle("esconder"); 
+});
+
+
+// Atividade 3
+let texto3 = document.getElementById("texto3");
+let contador = document.getElementById("contador")
+
+texto3.addEventListener("input", function() {
+    const quantidade = texto3.value.length;
+    contador.innerText = quantidade;
+  });
+
+
+// Atividade 4
+let clicar = document.getElementById("clicar");
+let contador2 = document.getElementById("contador2");
+nclique = 0;
+
+clicar.addEventListener("click", function() {
+    nclique = nclique + 1;
+    contador2.innerText = nclique; 
+})
+
+
+// Atividade 5
+let inserir = document.getElementById("inserir");
+let botao = document.getElementById("botao");
+let adicionar = document.getElementById("adicionar");
+
+botao.addEventListener("click", function() {
+    let novoItem = document.createElement("li");
+    novoItem.textContent = inserir.value;
+    adicionar.appendChild(novoItem);
+});
+
+
+// Atividade 6 + desafio extra
+let inserir2 = document.getElementById("inserir2");
+let botao2 = document.getElementById("botao2");
+let adicionar2 = document.getElementById("adicionar2");
+
+botao2.addEventListener("click", function() {
+    let novoItem2 = document.createElement("li");
+    novoItem2.textContent = inserir2.value;
+    adicionar2.appendChild(novoItem2);
+    novoItem2.addEventListener("click", function() {
+        if (confirm ("você quer MESMO apagar?")){
+            adicionar2.removeChild(novoItem2);
+        };
+    });
+});
+
+// Desafio HARD
+let texto4 = document.getElementById("texto4");
+let botao3 = document.getElementById("botao3");
+let mensagem = document.getElementById("mensagem");
+
+botao3.addEventListener("click", function(){
+    let novamensagem = document.createElement("p");
+    mensagem.innerText = texto4;
+    if (texto4 = ""){
+        alerta.classlist.toggle("vazio");
+        // o campo no nome é obrigatório
+    } else {
+        alerta.classList.toggle("preenchido");
+        // Nome enviado com sucesso
+    }
+    // arrumar a mensagem -> aparece com o alerta e no alerta
+})
