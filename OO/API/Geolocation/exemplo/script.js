9b64f6162885447ba86c9f8e400457724466ca95
Nananilda <anna.frezarini@aluno.senai.br>
2026-09-03 10:19:08 -0300

    Geolocation

--- OO/API/Geolocation/exemplo/script.js
+++ OO/API/Geolocation/exemplo/script.js
@@ -4,8 +4,14 @@ navigator.geolocation.getCurrentPosition(
         console.log("latitude:", posicao.coords.latitude);
         console.log("longitude:", posicao.coords.longitude);
         console.log("precisão:", posicao.coords.accuracy);
+
+        const resulatdo = document.querySelector("#resultado");
+        resulatdo.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy_27UkYpZCzo-5BO19IXPP9QLjCO0s-_r_NLdWm4t8w&s=10";
     },
     function (erro) {
         console.log("não foi possivel obter a localização", erro)
+
+        const resulatdo = document.querySelector("#resultado");
+        resulatdo.src = "https://cdn-icons-png.flaticon.com/512/2951/2951965.png";
     }
 );
