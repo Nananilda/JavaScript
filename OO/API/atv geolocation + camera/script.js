a8e445d477ac0a7bf9752f5812fc38586f8710f7
Nananilda <anna.frezarini@aluno.senai.br>
2026-09-03 12:28:57 -0300

    atividade

--- OO/API/atv geolocation + camera/script.js
+++ OO/API/atv geolocation + camera/script.js
@@ -1,20 +1,25 @@
 navigator.geolocation.getCurrentPosition(
     function (posicao) {
         const latitude = document.querySelector("#latitude");
-        latitude.add = posicao.coords.latitude;
+        latitude.innerHTML = " ", posicao.coords.latitude;
 
         const longitude = document.querySelector("#longitude");
-        longitude.add = posicao.coords.longitude;
+        longitude.innerHTML = " ", posicao.coords.longitude;
 
         const precisao = document.querySelector("#precisao");
-        precisao.add = posicao.coords.accuracy;
+        precisao.innerHTML = " ", posicao.coords.accuracy;
     },
 );
 
 navigator.mediaDevices.getUserMedia({
     video: true
 })
-.then(function(stream){
-    const video = document.querySelector("#camera");
-    video.srcObject = stream;
-})
+    .then(function (stream) {
+        const video = document.querySelector("#camera");
+        video.srcObject = stream;
+    })
+
+const botao = document.getElementById('botao');
+botao.addEventListener('click', function () {
+    alert('foto enviada!');
+});
