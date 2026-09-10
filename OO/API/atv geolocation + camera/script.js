da55ae67eee0664919073f87ac4d129928366752
Nananilda <anna.frezarini@aluno.senai.br>
2026-09-10 11:07:33 -0300

    atividade foto com registro

--- OO/API/atv geolocation + camera/script.js
+++ OO/API/atv geolocation + camera/script.js
@@ -1,3 +1,8 @@
+const video = document.querySelector("#camera");
+const canvas = document.querySelector("#canvas");
+const botao = document.querySelector("#botao");
+const foto = document.querySelector("#foto")
+
 navigator.geolocation.getCurrentPosition(
     function (posicao) {
         const latitude = document.querySelector("#latitude");
@@ -15,11 +20,25 @@ navigator.mediaDevices.getUserMedia({
     video: true
 })
     .then(function (stream) {
-        const video = document.querySelector("#camera");
         video.srcObject = stream;
     })
 
-const botao = document.getElementById('botao');
 botao.addEventListener('click', function () {
+    canvas.width = video.clientWidth;
+    canvas.height = video.clientHeight;
+
+    const contexto = canvas.getContext("2d");
+
+    contexto.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        canvas.height,
+    );
+    
+    foto.src = canvas.toDataURL("image/png");
+
     alert("foto enviada!");
 });
+
